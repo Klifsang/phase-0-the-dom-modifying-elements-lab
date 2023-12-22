7bc79ce3ba02f8c6878c95132ba6675d5b775509
Klif <sang.wicklife@student.moringaschool.com>
2023-12-22 09:39:32 +0300

    Assignments completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+var bodyElement = document.body;
+const mainNode = document.getElementById('main');
+mainNode.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Klif is the champion";
+
+// Use insertBefore to prepend the new child to the body
+bodyElement.insertBefore(newHeader, bodyElement.firstChild);
